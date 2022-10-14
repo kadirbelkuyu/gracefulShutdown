3b07862d67293e48b9c584e7eab6cc347a732e8d
kadirbelkuyu <kadirbelkuyu@gmail.com>
2022-10-14 16:55:45 +0300

    signal kısmı tamamlandı

---
+++ server_http.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "context"
+    "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
+
+    _ "github.com/gorilla/mux"
+)
+
+func main() {
+    fmt.Println("çalışıyor...")
+
+}
+
+
---
+++ signal.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "log"
+    "os"
+    "os/signal"
+    "syscall"
+    "fmt"
+)
+
+func main() {
+    done := make(chan os.Signal, 1)
+    signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+
+    <-done
+    log.Print("Sunucu Durduruldu")
+}
